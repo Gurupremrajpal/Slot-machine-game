0c2669e8f0dfa3035dc22b73a2a8bbeb78b68afa
Guruprem Rajpal <57023709+Gurupremrajpal@users.noreply.github.com>
2021-09-27 02:34:03 +0530

    Update script.js

--- Slot-machine-game/script.js
+++ Slot-machine-game/script.js
@@ -1,21 +1,13 @@
 $(".imgbtn").on("click", choosePic1);
 $(".restartbtn").on("click", restart);
+$(".changebtn").on("click", change);
 
 var images = new Array("img/lemon.png", "img/cherry.png", "img/bar.png", "img/dollar.png", "img/grapes.png", "img/orange.png", "img/seven.png");
 
 var amt;
 var reset = document.querySelector(".restartbtn").disabled =true;
 
-function restart() {
-  document.querySelector(".imgbtn").disabled = false;
-  document.querySelector("input").disabled = false;
-  document.querySelector(".restartbtn").disabled =true;
-  $(".message").html("");
-  $("input").val("");
-}
-
 function choosePic1() {
-
   amt = $("input").val();
 
   if(amt!=200)
@@ -24,7 +16,6 @@ function choosePic1() {
     document.querySelector(".imgbtn").disabled = true;
     document.querySelector("input").disabled = true;
     document.querySelector(".restartbtn").disabled =false;
-    
   }
   
   if(amt==200){
@@ -53,6 +44,26 @@ function choosePic3(){
   var audio = new Audio("audio/You-lose.mp3"); audio.play();
 }
 
+function change(){
+  if(confirm("Thank you for playing!! Now Let's Watch A Movie...")){
+  location.replace("https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjdhaf8zJzzAhVeILcAHR_UBjoQFnoECAgQAw&url=https%3A%2F%2Fwww.netflix.com%2F&usg=AOvVaw0qCEQCi5zeSVyTXooUjQV6")
+  }
+  else{
+      console.log("'Cancel' button was pressed.");
+  }
+}
+
 function showmessage(){
   $(".message").append("Congratulations ");
 }
+
+function restart() {
+  document.querySelector(".imgbtn").disabled = false;
+  document.querySelector("input").disabled = false;
+  document.querySelector(".restartbtn").disabled =true;
+  $(".message").html("");
+  $("input").val("");
+  location.reload();
+}
+
+
